4b7c9d8f245219d9b6e22a5d4199d0dc988d0431
lgaid <104643045+lgaid@users.noreply.github.com>
2022-08-30 13:08:06 -0400

    single responsibility principle good example

---
+++ src/com/cydeo/solid/singleResponsibility/example/good/CRUDService.java
@@ -0,0 +1,30 @@
+package com.cydeo.solid.singleResponsibility.example.good;
+
+public class CRUDService {
+
+
+    public static void create(){
+        // Create a user
+        // Send user to database related part
+    }
+
+    public static void read(){
+
+        // Read a user
+        // Send user to database related part
+
+    }
+
+    public static void update(){
+        // Update a user
+        // Send user to database related part
+    }
+
+    public static void delete(){
+         // Delete  user
+        // Send user to database related part
+
+    }
+
+
+}
