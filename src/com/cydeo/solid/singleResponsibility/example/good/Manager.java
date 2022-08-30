c1c72d22d50aa6074c984ff409b722e2cb167ff8
lgaid <104643045+lgaid@users.noreply.github.com>
2022-08-30 13:16:13 -0400

    Repository added

---
+++ src/com/cydeo/solid/singleResponsibility/example/good/Manager.java
@@ -0,0 +1,18 @@
+package com.cydeo.solid.singleResponsibility.example.good;
+
+public class Manager {
+
+    public static void boolean validate(){
+        // Check validation
+        return true;
+
+    }
+
+
+    public static void boolean  checkSecurity(){
+        // Send to Security related part
+        return true;
+
+    }
+
+}
