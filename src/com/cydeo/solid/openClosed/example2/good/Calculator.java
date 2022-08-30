8bd29762a4d707a2cc6e94b2baed1e0e09be476a
lgaid <104643045+lgaid@users.noreply.github.com>
2022-08-30 13:46:57 -0400

    Calculator added

---
+++ src/com/cydeo/solid/openClosed/example2/good/Calculator.java
@@ -0,0 +1,16 @@
+package com.cydeo.solid.openClosed.example2.good;
+
+public class Calculator {
+
+    public static void calculator(Operation operation){
+
+        int num1 = 10;
+        int num2 = 2;
+
+        operation.calculate(num1,num2);
+
+    }
+
+
+
+}
