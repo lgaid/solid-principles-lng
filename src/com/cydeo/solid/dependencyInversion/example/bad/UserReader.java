683c695ec8f7d9e8c2d516f98f9d66af699262c6
lgaid <104643045+lgaid@users.noreply.github.com>
2022-08-30 15:34:40 -0400

    fixes

--- src/com/cydeo/solid/dependencyInversion/example/bad/UserReader.java
+++ src/com/cydeo/solid/dependencyInversion/example/bad/UserReader.java
@@ -3,13 +3,19 @@ package com.cydeo.solid.dependencyInversion.example.bad;
 public class UserReader {
 
     private XMLReader xmlReader;            // What happens if we decide to use JSON format instead of XML format?
-
-    public UserReader(XMLReader xmlReader) {
+    // private XMLReade jsonReader;
+    public UserReader(XMLReader xmlReader) {    // We need to update json reader in constructor too
         this.xmlReader = xmlReader;
     }
 
     public String getUsername() {
-        return xmlReader.getUsername();
+        return xmlReader.getUsername();     // public string getUserNameFromXML()
+    }
+
+
+    public String getUsernameFromJson() {
+        // return JsonReader.getUserName;
+        return xmlReader.getUsername();     // public string getUserNameFromXML()
     }
 
 }
