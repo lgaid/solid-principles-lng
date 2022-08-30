0fc86b208459a5470be2897e4a04ddbd871c676f
lgaid <104643045+lgaid@users.noreply.github.com>
2022-08-30 13:18:42 -0400

    Security added

---
+++ src/com/cydeo/solid/singleResponsibility/example/good/SecurityFilter.java
@@ -0,0 +1,18 @@
+package com.cydeo.solid.singleResponsibility.example.good;
+
+public class SecurityFilter {
+
+
+    public static boolean authentication (){
+        //Check authentication
+        return true;
+
+    }
+
+    public static boolean authorization (){
+        // Check authorization
+        return true;
+
+    }
+
+}
